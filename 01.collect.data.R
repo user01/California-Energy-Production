26cd06b53886f34e7169d3a2951fbe1abca84bce
Erik <erik.langenborg@gmail.com>
2016-11-19 13:53:13 -0500

    Processing epa data into common format

---
+++ 01.collect.data.R
@@ -0,0 +1,54 @@
+
+suppressPackageStartupMessages( {
+  library(purrr)
+  library(dplyr)
+  library(lubridate)
+  library(readr)
+  library(stringr)
+})
+
+
+file_paths <- list.files("epa", recursive=TRUE, pattern=".zip")
+
+read_epa_data <- function(path){
+  path %>%
+    read_csv(col_types = cols(
+      STATE = col_character(),
+      FACILITY_NAME = col_character(),
+      ORISPL_CODE = col_integer(),
+      UNITID = col_character(),
+      OP_DATE = col_character(),
+      OP_HOUR = col_integer(),
+      OP_TIME = col_double(),
+      `GLOAD (MW)` = col_integer(),
+      `SO2_MASS (lbs)` = col_double(),
+      `SO2_RATE (lbs/mmBtu)` = col_double(),
+      `NOX_RATE (lbs/mmBtu)` = col_double(),
+      `NOX_MASS (lbs)` = col_double(),
+      `CO2_MASS (tons)` = col_double(),
+      `CO2_RATE (tons/mmBtu)` = col_double(),
+      `HEAT_INPUT (mmBtu)` = col_double(),
+      FAC_ID = col_integer(),
+      UNIT_ID = col_integer()
+    )) %>%
+    mutate(
+        DATE = fast_strptime(paste(OP_DATE, OP_HOUR),
+                             "%m-%d-%Y %H",
+                             tz = "UTC",
+                             lt = FALSE)
+      ) %>%
+    select(-STATE, -OP_DATE, -OP_HOUR)
+}
+
+
+file_paths %>%
+  map_chr(~ file.path("epa", .)) %>%
+  map(read_epa_data) %>%
+  reduce(rbind) %>%
+  mutate(
+      FACILITY_NAME = as.factor(FACILITY_NAME)
+    )->
+  data_results
+# data_results %>% glimpse
+
+data_results %>% saveRDS(file.path("rds", "epa.results.rds"))
