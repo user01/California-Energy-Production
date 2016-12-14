38586346ab50734a5092c148739c6fec055bed9a
Erik <erik.langenborg@gmail.com>
2016-12-14 02:41:35 -0500

    Toying with model work

---
+++ model.simple.R
@@ -0,0 +1,141 @@
+
+
+suppressPackageStartupMessages( {
+  library(purrr)
+  library(dplyr)
+  library(lubridate)
+  library(readr)
+  library(stringr)
+  library(randomForest)
+})
+
+generator_data_daily <- read_rds(file.path("rds", "generator_data_daily.rds"))
+generator_data_daily %>% glimpse
+
+
+generator_data_daily %>%
+  filter(ENERGY_SOURCE == "NG") %>%
+  group_by(PLANT_CODE, DATE) %>%
+  mutate(ACTIVE = OP_TIME > 0.5) ->
+  generators_clean
+
+generators_clean %>%
+  # select(DATE, PLANT_CODE, FACILITY_NAME, ACTIVE, GRIDVOLTAGE, TEMP_F_MAX, TEMP_F_MIN, TEMP_F_MEAN) %>%
+  glimpse
+
+# generators_clean %>%
+#   get("ACTIVE", .) %>%
+#   unlist() %>%
+#   sum(na.rm=T) %>%
+#   glimpse
+
+generators_clean %>%
+  filter(PLANT_CODE == 228) %>%
+  mutate(ACTIVE = as.factor(ACTIVE)) %>%
+  ungroup() ->
+  costa
+
+
+# Note, 60k / 222k are true. Uneven, but not outrageously so
+
+
+if_else_ <- function(.data, .truth, .lhs, .rhs) {
+  if (.truth) {
+    .lhs(.data)
+  } else {
+    .rhs(.data)
+  }
+}
+
+factor_scores <- function(predictions, truths) {
+  truths %>%
+    unique %>%
+    map(function(fct) {
+      list(truths, predictions) %>%
+        transpose %>%
+        map(unlist) %>%
+        discard(~ .[1] != as.numeric(fct)) -> truths_and_predictions
+
+      truths_and_predictions %>%
+        length ->
+        predictions_total
+
+      truths_and_predictions %>%
+        keep(~ .[1] == .[2]) %>%
+        length ->
+        predictions_correct
+
+      predictions_total - predictions_correct ->
+        predictions_wrong
+
+      data.frame(predictions_total, predictions_correct, predictions_wrong) %>%
+        `colnames<-`(paste(as.character(fct), c("total", "correct", "wrong"), sep = "_"))
+
+    }) %>%
+    reduce(cbind)
+}
+
+k_fold_prob <- function(data, current_formula, k = 10, resample = TRUE) {
+  folds <- cut(seq(1, nrow(data)), breaks = k, labels = FALSE) %>% if_else_(resample,
+    sample, identity)
+  # NOTE: call to sample is random. For consistent behavior, use set.seed
+
+  1:k %>%
+    purrr::map(function(fold_number) {
+      temp.train <- data %>% filter(folds != fold_number)
+      temp.test <- data %>% filter(folds == fold_number)
+
+      current_formula %>%
+        randomForest(data = temp.train, importance=TRUE, ntree=200) %>%
+        predict(temp.test) %>%
+        factor_scores(temp.test$ACTIVE) # TODO: remove this hard coded factor
+    }) %>%
+    purrr::reduce(rbind) %>%
+    dmap(sum)
+}
+
+res <- randomForest(ACTIVE ~ GRIDVOLTAGE + OP_TIME, data = costa, importance=TRUE, ntree=200)
+# varImpPlot(res)
+
+# Source: http://stackoverflow.com/a/39894310/2601448
+getSeason <- function(input.date){
+  numeric.date <- 100*month(input.date)+day(input.date)
+  ## input Seasons upper limits in the form MMDD in the "break =" option:
+  cuts <- base::cut(numeric.date, breaks = c(0,319,0620,0921,1220,1231))
+  # rename the resulting groups (could've been done within cut(...levels=) if "Winter" wasn't double
+  levels(cuts) <- c("Winter","Spring","Summer","Fall","Winter")
+  return(cuts)
+}
+
+
+costa %>%
+  # head(50) %>%
+  mutate(
+    OP_TIME_LAG = lag(OP_TIME, 1),
+    wday = as.factor(wday(DATE, label = TRUE)),
+    wend = wday == "Sat" | wday == "Sun",
+    season = getSeason(DATE)
+  ) %>%
+  na.omit() ->
+  costa_featured
+
+costa_featured %>%
+  glimpse
+
+
+
+set.seed(0451)
+k_fold_prob(costa_featured, ACTIVE ~ GRIDVOLTAGE + TEMP_F_MEAN)
+k_fold_prob(costa_featured, ACTIVE ~ GRIDVOLTAGE + TEMP_F_MEAN + wday + wend + season + OP_TIME_LAG)
+
+
+k_fold_prob(costa, ACTIVE ~ GRIDVOLTAGE + TEMP_F_MEAN + OP_TIME)
+
+
+folds <- cut(seq(1, nrow(costa)), breaks = 5, labels = FALSE) %>% if_else_(T,
+  sample, identity)
+
+folds %>% glimpse
+
+temp_train <- costa %>% ungroup() %>% filter(folds != 1)
+temp_test <- costa %>% filter(folds == 1)
